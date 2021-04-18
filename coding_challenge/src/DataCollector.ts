aa101dcc58eb9b891804d40eede97eed7a5d41a7
Enis <eniszej@gmail.com>
2021-04-18 20:24:59 +0200

    fixed DataCollector

--- coding_challenge/src/DataCollector.ts
+++ coding_challenge/src/DataCollector.ts
@@ -5,6 +5,8 @@ export class DataCollector {
   private static instance: DataCollector;
   endpoint = "https://rickandmortyapi.com/api";
 
+  public MAX_PAGE_EPISODES = 0;
+  public MAX_PAGE_CHARACTERS = 0;
   private constructor() {}
 
   public static getInstance(): DataCollector {
@@ -15,29 +17,47 @@ export class DataCollector {
     return DataCollector.instance;
   }
 
-  getCharacters = async (page:number): Promise<Character[]> => {
-    const response = await fetch(this.endpoint + "/character/?page="+page).then((data) => {
+  getCharacters = async (page: number): Promise<Character[]> => {
+    const response = await fetch(
+      this.endpoint + "/character/?page=" + page
+    ).then((data) => {
       return data.json();
     });
+    if (this.MAX_PAGE_CHARACTERS === 0) {
+      this.MAX_PAGE_CHARACTERS = response.info.pages;
+    }
     return response.results;
   };
 
-  getEpisodes = async (page:number): Promise<Episode[]> => {
-    const response = await fetch(this.endpoint + "/episode/?page="+page).then((data) => {
-      return data.json();
-    });
+  getEpisodes = async (page: number): Promise<Episode[]> => {
+    const response = await fetch(this.endpoint + "/episode/?page=" + page).then(
+      (data) => {
+        return data.json();
+      }
+    );
+    if (this.MAX_PAGE_CHARACTERS === 0) {
+      this.MAX_PAGE_EPISODES = response.info.pages;
+    }
     return response.results;
   };
 
   getCharacterById = async (id: number) => {
-    const response = await fetch(this.endpoint + "/episode/"+id).then((data) => {
-      return data.json();
-    });
-    return response.results;
-  }
+    const response = await fetch(this.endpoint + "/character/" + id).then(
+      (data) => {
+        return data.json();
+      }
+    );
+    return response;
+  };
 
-  getEpisodeById(id: number) {}
-
+  getEpisodeById = async (id: number) => {
+    const response = await fetch(this.endpoint + "/episode/" + id).then(
+      (data) => {
+        return data.json();
+      }
+    );
+    return response;
+  };
 }
 
 export const dataCollector = DataCollector.getInstance();
