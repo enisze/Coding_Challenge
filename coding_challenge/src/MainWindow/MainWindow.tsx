12f5447fbb2990f354f70456825c41a96b34022a
Enis <eniszej@gmail.com>
2021-04-20 19:05:38 +0200

    Added MainWindow text

--- coding_challenge/src/MainWindow/MainWindow.tsx
+++ coding_challenge/src/MainWindow/MainWindow.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { DashboardLayout } from "../Components/DashboardLayout";
+import { ItemText } from "../Components/StyledComponents";
 
 export const MainWindow: React.FC = (props) => {
   return (
     <DashboardLayout>
-      <div>Welcome, 
-        on this page you can browse the Characters and Episodes of Rick and Morty.
-
-        Enjoy!
+      <div style={{ paddingBottom: "2rem", paddingTop: "1rem" }}>Welcome, </div>
+      <div style={{ paddingBottom: "2rem" }}>
+        on this page you can browse
+        <div>the Characters and Episodes</div>
+        <div>of Rick and Morty.</div>
       </div>
+      <div>Enjoy!</div>
     </DashboardLayout>
   );
 };
